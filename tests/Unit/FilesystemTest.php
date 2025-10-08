9cb54d20ae564f086da8fbbd57f509385a1fa834
Jasper Zonneveld <jasper@swis.nl>
2025-10-08 12:13:13 +0200

    test: move filesystem setup to specific test case

--- tests/Unit/FilesystemTest.php
+++ tests/Unit/FilesystemTest.php
@@ -3,12 +3,20 @@
 namespace Swis\Laravel\Encrypted\Tests\Unit;
 
 use Illuminate\Support\Facades\Storage;
+use Orchestra\Testbench\Attributes\DefineEnvironment;
 use PHPUnit\Framework\Attributes\Test;
 use Swis\Laravel\Encrypted\Tests\TestCase;
 
 final class FilesystemTest extends TestCase
 {
+    protected function usesEncryptedDisk($app): void
+    {
+        $app['config']->set('filesystems.default', 'local');
+        $app['config']->set('filesystems.disks.local', ['driver' => 'local-encrypted', 'root' => dirname(__DIR__).'/_files/']);
+    }
+
     #[Test]
+    #[DefineEnvironment('usesEncryptedDisk')]
     public function itRegistersTheFilesystemDriver(): void
     {
         $contents = Storage::get('read.txt');
